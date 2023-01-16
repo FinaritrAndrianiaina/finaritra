06e1a772e0e1e504dea6d1fe4be680f1681e8739
Finaritra <finaritra.andrianiaina@pulse.mg>
2023-01-16 17:19:54 +0300

    fix sticky on time

--- src/lib/components/timeline/Time.svelte
+++ src/lib/components/timeline/Time.svelte
@@ -2,7 +2,7 @@
 
 <div class=" border-l-2 border-secondary translate-x-0.5">
 	<div  class="flex">
-		<h1 class="relative text-lg top-0 h-fit p-1  px-6">
+		<h1 class="sticky text-lg top-0 h-fit p-1  px-6">
 			<slot name="date" />
 		</h1>
 		<div class="flex flex-col w-full">
